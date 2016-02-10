47c031a47c3d1e489112c16a752357441fa8bdb7
Suad <suadbasbug@gmail.com>
2016-02-09 16:58:19 -0800

    Bu cizim araci cok guzel. o boy o boy...

--- LAAS_V1/src/silisyum/Basladik_Galiba.java
+++ LAAS_V1/src/silisyum/Basladik_Galiba.java
@@ -1,46 +1,81 @@
 package silisyum;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
-public class Basladik_Galiba extends JFrame {
+public class Basladik_Galiba extends ApplicationFrame {
 
-	/**
+/**
 	 * 
 	 */
-	private static final long serialVersionUID = -7314526871078291921L;
-	private JPanel contentPane;
+	private static final long serialVersionUID = -6489772081065435316L;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Basladik_Galiba frame = new Basladik_Galiba();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+/**
+* A demonstration application showing an XY series containing a null value.
+*
+* @param title  the frame title.
+*/
+public Basladik_Galiba(final String title) {
 
-	/**
-	 * Create the frame.
-	 */
-	public Basladik_Galiba() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+   super(title);
+   final XYSeries series = new XYSeries("Random Data");
+   series.add(1.0, 500.2);
+   series.add(5.0, 694.1);
+   series.add(4.0, 100.0);
+   series.add(12.5, 734.4);
+   series.add(17.3, 453.2);
+   series.add(21.2, 500.2);
+   series.add(21.9, null);
+   series.add(25.6, 734.4);
+   series.add(30.0, 453.2);
+   final XYSeriesCollection data = new XYSeriesCollection(series);
+   final JFreeChart chart = ChartFactory.createXYLineChart(
+       "XY Series Demo",
+       "X", 
+       "Y", 
+       data,
+       PlotOrientation.VERTICAL,
+       true,
+       true,
+       false
+   );
+
+   final ChartPanel chartPanel = new ChartPanel(chart);
+   chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+   setContentPane(chartPanel);
+
+}
+
+//****************************************************************************
+//* JFREECHART DEVELOPER GUIDE                                               *
+//* The JFreeChart Developer Guide, written by David Gilbert, is available   *
+//* to purchase from Object Refinery Limited:                                *
+//*                                                                          *
+//* http://www.object-refinery.com/jfreechart/guide.html                     *
+//*                                                                          *
+//* Sales are used to provide funding for the JFreeChart project - please    * 
+//* support us so that we can continue developing free software.             *
+//****************************************************************************
+
+/**
+* Starting point for the demonstration application.
+*
+* @param args  ignored.
+*/
+public static void main(final String[] args) {
+
+   final Basladik_Galiba demo = new Basladik_Galiba("XY Series Demo");
+   demo.pack();
+   RefineryUtilities.centerFrameOnScreen(demo);
+   demo.setVisible(true);
+
+}
 
 }
