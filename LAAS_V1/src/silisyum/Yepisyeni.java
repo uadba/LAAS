0e388cf36f709f31f1580d2878ccbf215c4824c8
Suad <suadbasbug@gmail.com>
2016-02-12 15:02:35 -0800

    Ortalik karisti

--- LAAS_V1/src/silisyum/Yepisyeni.java
+++ LAAS_V1/src/silisyum/Yepisyeni.java
@@ -9,10 +9,67 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JToggleButton;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JToolBar;
+import javax.swing.JRadioButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
+import javax.swing.JDesktopPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SpringLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
 
 public class Yepisyeni extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField yazi;
+	private JList list;
+		
+	/**
+	 * Create the frame.
+	 */
+	public Yepisyeni() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
+		contentPane.add(btnNewButton);
+		
+		yazi = new JTextField();
+		contentPane.add(yazi);
+		yazi.setColumns(10);
+		
+		list = new JList();
+		contentPane.add(list);
+	}
 
 	/**
 	 * Launch the application.
@@ -29,26 +86,4 @@ public class Yepisyeni extends JFrame {
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Yepisyeni() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				System.out.println("Tıklatıldı");
-			}
-		});
-		contentPane.add(btnNewButton, BorderLayout.CENTER);
-	}
-
 }
